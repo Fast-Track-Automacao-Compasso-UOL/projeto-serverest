fb9edd60fc801e458afe292e6e2716693aff290c
Lucas Gomes <lucasgdo4@gmail.com>
2021-06-04 17:56:19 -0300

    Implementando GET /usuarios

---
+++ cypress/services/serveRest.service.js
@@ -0,0 +1,57 @@
+/// <reference types="cypress" />
+import Rest from "./_rest.service"
+
+export class ServeRest extends Rest {
+  // Armazena rota baseado no parâmetro recebido
+  static armazenarRota(rota) {
+    cy.wrap(rota).as('Rota');
+  }
+
+  // Adiciona query params à rota recebida pelo cy.wrap()
+  static adicionarQueryParams(params) {
+    cy.get('@Rota').then(rota => {
+      switch (params) {
+        case "_id":
+          cy.wrap(`${rota}?_id=0uxuPY0cbmQhpEz1`).as('Rota')
+          break;
+        case "nome":
+          cy.wrap(`${rota}?nome=Fulano%20da%20Silva`).as('Rota')
+          break;
+        case "email":
+          cy.wrap(`${rota}?email=fulano@qa.com`).as('Rota')
+          break;
+        case "password":
+          cy.wrap(`${rota}?password=teste`).as('Rota')
+          break;
+        case "administrador":
+          cy.wrap(`${rota}?administrador=true`).as('Rota')
+          break;
+        default:
+          cy.wrap(rota).as('Rota')
+          break;
+      }
+    })
+  }
+
+  // Realiza requisição com rota recebida pelo cy.wrap() e tipo recebido no parâmetro
+  static realizarRequisicao(tipo) {
+    cy.get('@Rota').then(rota => {
+      switch (tipo) {
+        case "GET":
+          Rest.get(rota).then(res => {
+            cy.wrap(res.body).as('Body')
+          })
+          break;
+        default:
+          break;
+      }
+    })
+  }
+
+  // Valida schema e status com body da requisição recebido pelo cy.wrap() e parâmetros
+  static validarSchemaEStatus(schema, status) {
+    cy.get('@Body').then(body => {
+      cy.validateSchema(body, `${schema}/${status}`)
+    })
+  }
+}
