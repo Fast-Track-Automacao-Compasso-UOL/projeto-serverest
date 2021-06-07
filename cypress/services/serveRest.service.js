96a118c4f4b497bbde22f931d91b26a40dd3c972
Filipe Conde <filipecondep@gmail.com>
2021-06-07 17:03:25 -0300

    cria arquivo fixture para teste de Login

--- cypress/services/serveRest.service.js
+++ cypress/services/serveRest.service.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import Rest from './_rest.service';
+import req_body from '../fixtures/login/req_body.json'
 
 const URL_BASE = Cypress.config("baseUrl");
 const URL_USUARIOS = "/usuarios";
@@ -11,11 +12,12 @@ const URL_CARRINOS = "/carrinhos";
 export class ServeRest extends Rest {
 
     static realizar_login() {
+        //ajustar seleção de body
         let body = {
             "email": "fulano@qa.com",
             "password": "teste"
-          };
-        
+        }
+            
         super.post(URL_BASE + URL_LOGIN, body).then(res => {
             cy.wrap(res.body).as('body');
         });
