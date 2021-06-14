3890d9d146bdfbb3e86db607676885fa9eb093e0
Lucas Gomes <lucasgdo4@gmail.com>
2021-06-14 17:25:10 -0300

    Removendo método de bucarDadosUsuario

--- cypress/services/usuarios.service.js
+++ cypress/services/usuarios.service.js
@@ -4,20 +4,6 @@ import { criarBodyUsuario } from '../factories/dynamic';
 const URL_USUARIOS = "/usuarios";
 
 export class Usuarios extends ServeRest {
-  // Busca dados de usuários já existentes e envia pelo cy.wrap()
-  static buscarDadosUsuario(tipo = false) {
-    let admin;
-    if (tipo == 'admin') {
-      admin = true;
-    } else {
-      admin = false;
-    }
-    super.get(`${URL_USUARIOS}?administrador=${admin}`).then((res) => {
-      cy.wrap(res.body.usuarios[0].email).as("Email");
-      cy.wrap(res.body.usuarios[0].password).as("Password");
-    });
-  }
-
   // Cria usuário(admin ou não), baseado no parâmetro recebido
   static criarUsuario(options = { admin: 'false' }) {
     super.post(URL_USUARIOS, criarBodyUsuario({ administrador: options.admin })).then(res => {
